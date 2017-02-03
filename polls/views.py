ac819954bbb2c941e7ce2e35c5f23e704b7e319e
anatekar <amol.natekar@gmail.com>
2017-02-03 16:29:26 +0530

    add test

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,26 @@ from django.http import HttpResponseRedirect,HttpResponse
 from django.shortcuts import get_object_or_404,render
 from .models import Choice,Question
 from django.urls import reverse
-from django.http import Http404
+from django.views import generic
+from django.utils import timezone
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """ Return the last five published questions """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def index(request):
@@ -39,4 +58,5 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
 
